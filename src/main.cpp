332877847d99827ebae1298691c34aeac91bb1f5
zMidair <midair4546@gmail.com>
2024-07-17 12:42:42 +0300

    added github link + prevention from holding key

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,22 @@
 using namespace geode::prelude;
 
 auto* GM = GameManager::sharedState();
+bool keyDown = false;
 
 struct ModifiedCCKeyboardDispatcher : Modify<ModifiedCCKeyboardDispatcher, CCKeyboardDispatcher>
 {
 	bool dispatchKeyboardMSG(enumKeyCodes key, bool down, bool arr)
 	{
-		if(down && key == KEY_F11)
+		if(down && key == KEY_F11 && !keyDown)
 		{
+			// Prevent the user from holding the F11 key, crashing the game
+			keyDown = down;
+
 			bool CurrentValue = GM->getGameVariable("0025");
 			GM->setGameVariable("0025", !CurrentValue);
 			GM->reloadAll(true, CurrentValue, true);
 		}
+		if(!down && key == KEY_F11 && keyDown) keyDown = down;
 
 		return CCKeyboardDispatcher::dispatchKeyboardMSG(key, down, arr);
 	}
